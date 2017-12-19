a4afc322313e2776eb8394422a92fbb47ea75ac6
Samantha <sla1031@gmail.com>
2017-12-18 20:04:47 -0500

    remove depulicate google_calendar

--- src/components/google_calendar/google_calendar.js
+++ src/components/google_calendar/google_calendar.js
@@ -8,7 +8,7 @@ export default class GoogleSearch extends React.Component {
       <div className="google-calendar-container">
         <iframe
           title="Google Calendar"
-          src="https://calendar.google.com/calendar/b/1/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=samantha%40digitalrelab.com&amp;color=%232952A3&amp;src=13.tragic.with.a.capital.t.22%40gmail.com&amp;color=%23875509&amp;ctz=America%2FNew_York"
+          src="" // google calendar source here
           scrolling="no"
         />
       </div>
